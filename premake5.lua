f21ce37af2bf0a3f7e80a09f769c5da3326a2103
no-lith <113048918+no-lith@users.noreply.github.com>
2024-12-02 02:41:07 -0300

    Add ASAN
    - Warning level to 4
    - All warnings as errors
    - ASAN in debug builds
    - Multi processor compilation

--- premake5.lua
+++ premake5.lua
@@ -2,45 +2,53 @@ workspace "RezExtract"
     configurations { "Debug", "Release" }
     platforms { "Win32", "Win64" }
 
-    project "RezExtract"
-        kind "ConsoleApp"
-        language "C++"
-        cppdialect "C++20"
-        system "Windows"
+project "RezExtract"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++20"
+    system "Windows"
 
-        pchheader "pch.hpp"
-        pchsource "src/pch.cpp"
-        
-        targetname "RezExtract"
+    pchheader "pch.hpp"
+    pchsource "src/pch.cpp"
+    
+    targetname "RezExtract"
 
-        objdir ( "bin/intermediates/%{cfg.architecture}-%{cfg.buildcfg}" )
-        targetdir ( "bin/%{cfg.architecture}-%{cfg.buildcfg}" )
+    objdir ( "bin/intermediates/%{cfg.architecture}-%{cfg.buildcfg}" )
+    targetdir ( "bin/%{cfg.architecture}-%{cfg.buildcfg}" )
 
-        staticruntime "on"
+    staticruntime "on"
 
-        files
-        {
-            "inc/*.hpp",
-            "src/*.cpp"
-        }
+    files
+    {
+        "inc/*.hpp",
+        "src/*.cpp"
+    }
 
 
-        includedirs
-        {
-            "inc"
-        }
+    includedirs
+    {
+        "inc"
+    }
 
-        filter { "platforms:Win32" }
-            architecture "x86"
+    symbols "On"
+    flags { "FatalWarnings", "MultiProcessorCompile" }
+    warnings "High"
 
-        filter { "platforms:Win64" }
-            architecture "x86_64"
+    filter {"platforms:Win32"}
+        architecture "x86"
 
-        filter { "configurations:Debug" }
-            defines { "DEBUG" }
-            symbols "On"
+    filter {"platforms:Win64"}
+        architecture "x86_64"
+    
+    filter {}
 
-        filter { "configurations:Release" }
-            defines { "NDEBUG" }
-            optimize "Speed"
-            symbols "Off"
+    filter "Debug"
+        defines "DEBUG"
+        editandcontinue "Off"
+        flags "NoIncrementalLink"
+        sanitize "Address"
+        optimize "Off"
+
+    filter "Release"
+        defines "NDEBUG"
+        optimize "Full"
